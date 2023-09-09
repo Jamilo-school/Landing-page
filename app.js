a8f0a45986ce84075c2c83a53a8b4e2caf9e72c9
Jamilo-school <geofreyonyango167@gmail.com>
2023-09-09 22:38:52 +0300

    push

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navbar.classList.toggle("active");
 })
-document.querySelectorAll(".link1","link2","link3","link4").forEach(n => n.
+document.querySelectorAll("nav").forEach(n => n.
    addEventListener("click", () => {
     hamburger.classList.remove("active")
     navbar.classList.remove("active")
