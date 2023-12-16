5e92e306f150ef62806d95c5c05c167874592908
Jamilo-school <geofreyonyango167@gmail.com>
2023-12-15 20:48:52 -0800

    ihihihihihi

--- HeadTeachers Office/filterNames.js
+++ HeadTeachers Office/filterNames.js
@@ -1,11 +1,14 @@
+const onSearch = () => {
+  const input = document.querySelector("#search");
+  const filter = input.value.toUpperCase();
 
-const onSearch = () =>{
-    const input = document.querySelector("#search");
-    const filter = input.value.toUpperCase();
-  
-    const list = document.querySelectorAll("#list li");
-    list.forEach((el) =>{
-  const text = el.textContent.toUpperCase();
-  el.style.display = text.includes(filter) ?"":"none";
-    });
-  };
+  const list = document.querySelectorAll("#list li");
+  list.forEach((el) => {
+    const text = el.textContent.toUpperCase();
+    if (text.includes(filter)) {
+      el.classList.add("show");
+    } else {
+      el.classList.remove("show");
+    }
+  });
+};
